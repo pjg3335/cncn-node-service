8bddd06889cf17e30edf76b519bcc2df8ec7e2ae
pjg3335 <jje3335@naver.com>
2025-06-10 21:08:15 +0900

    feat: [BVFH-241] id로 bulk조회하는 api 추가

---
+++ apps/bff-server/src/main.ts
@@ -0,0 +1,43 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { BigIntToNumberInterceptor, ZodExceptionFilter } from '@app/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
+import type { Request, Response } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.enableCors({
+    origin: /^http:\/\/localhost(:\d+)?$/,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
+
+  app.useGlobalFilters(new ZodExceptionFilter());
+
+  app.useGlobalInterceptors(new BigIntToNumberInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API에 대한 설명')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addServer('/bff-service')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.use('/v3/api-docs', (_: Request, res: Response) => res.json(document));
+  SwaggerModule.setup('/swagger-ui', app, () => document, {
+    swaggerUrl: '/v3/api-docs',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
