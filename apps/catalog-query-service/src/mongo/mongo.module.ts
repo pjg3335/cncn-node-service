16c45b4c15fc07ac7df0d3095fe84183a5401f59
pjg3335 <jje3335@naver.com>
2025-07-07 20:16:44 +0900

    hotfix: rollback

--- apps/catalog-query-service/src/mongo/mongo.module.ts
+++ apps/catalog-query-service/src/mongo/mongo.module.ts
@@ -11,12 +11,10 @@ import { EnvSchema } from '../common/env-schema';
       provide: 'MONGO_DB',
       inject: [ConfigService],
       useFactory: async (configService: ConfigService<EnvSchema>) => {
-        const baseUrl = configService.getOrThrow('MONGO_URL');
-        const dbName = 'catalog-query-service';
-        const [uri, query] = baseUrl.split('?');
-        const fullUrl = `${uri}/${dbName}${query ? `?${query}` : ''}`;
-        const client = await MongoClient.connect(fullUrl);
-        return client.db(dbName);
+        const url = new URL(configService.getOrThrow('MONGO_URL'));
+        url.pathname = 'catalog-query-service';
+        const client = await MongoClient.connect(url.toString());
+        return client.db('catalog-query-service');
       },
     },
   ],
